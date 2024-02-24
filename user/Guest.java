f5958945648316bee7f0604133a2b5d5be5fc9e6
Yousef Ali <132370936+YousefAli-1@users.noreply.github.com>
2024-02-24 10:24:04 +0200

    Add files via upload

---
+++ user/Guest.java
@@ -0,0 +1,87 @@
+package user;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import Employees.Person;
+import javax.management.InvalidAttributeValueException;
+import system.Category;
+import system.Reservation;
+public class Guest extends Person {
+    private static ArrayList<Guest> Guests=new ArrayList<Guest>();
+    Category PreferredCategory;
+    int numOfReservations=0;
+   public Guest(String Name, String Address, String DateOfBirth, String PhoneNum, String Email,String UserName,String Password){
+        super(Name,Address,DateOfBirth,PhoneNum,Email,UserName,Password);
+        Guests.add(this);
+    }
+      public  String ViewReservation(){
+       String List=Reservation.search(this).toString();
+       return List;
+    }
+    public void incrementReservation(){
+        numOfReservations++;
+    }
+    public void decrementReservation(){
+        numOfReservations--;
+    }
+    public void setPreferedCategory(int key){
+        switch(key){
+            case 0:
+                PreferredCategory=Category.Standard;
+                break;
+            case 1:
+                PreferredCategory=Category.Couples;
+                break;
+            case 2:
+                PreferredCategory=Category.Family;
+                break;
+            case 3:
+                PreferredCategory=Category.Private;
+                break;        
+
+        }
+    }
+    public static Guest getGuest(int key){
+        for(int i=0;i<Guests.size();i++){
+            if(key==Guests.get(i).getId()){
+                return Guests.get(i);
+            }
+        }
+        return null;
+    }
+    public static ArrayList<Guest> getList(){
+     return Guests;
+    } 
+    public static void saveRecords(){
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("GuestsData.dat"));
+                out.writeObject(Guests);
+            out.close();
+            } catch (IOException e) {
+            System.out.println(e);
+            }
+    }
+    public static void getRecord(){
+        try {
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("GuestsData.dat"));
+            Guests=(ArrayList<Guest>)in.readObject();
+            in.close();
+        }catch (ClassNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+    public void RateBooking(byte Rating) throws InvalidAttributeValueException{
+    Reservation.search(this).getLast().setRating(Rating);
+}    
+    @Override
+    public String toString() {
+        String history=ViewReservation();
+        return "Guest{history= " + history + ", PreferredCategory=" + PreferredCategory + '}';
+    }
+}            
+
